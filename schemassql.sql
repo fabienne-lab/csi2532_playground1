aad15d6af12e297a422363ea5a478ca537eb733c
Fabienne Sawadogo <msawa100@uottawa.ca>
2021-03-01 17:48:16 -0500

    lab5 sur les migrations

---
+++ schemassql.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Athletes (
+id int(5),
+name varchar(300),
+birthday varchar(10),
+gender varchar(6),
+inscription date,
+PRIMARY KEY (id)
+);
---
+++ seed.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Athletes (
+id int(5),
+name varchar(300),
+birthday varchar(10),
+gender varchar(6),
+PRIMARY KEY (id)
+);
+
+
+INSERT INTO Athletes (id, name, birthday, gender)
+VALUES
+(02456, 'Fabtal', 'F', '08/12/2000'),
+(01915, 'Zoumana', 'F', '07/09/1997'),
+(05066, 'Bintou', 'M', '29/11/1999'),
+(01915, 'Lalé', 'F', '15/05/2002');
